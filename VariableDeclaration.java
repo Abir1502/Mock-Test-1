7aa44ca06d73a30cf6606c4fb4ffe47ebbb4e400
Abir1502 <abirbanerjee1502@gmail.com>
2023-07-03 13:38:35 +0530

    To declare and initialize variables of different data types with appropriate identifiers

---
+++ VariableDeclaration.java
@@ -0,0 +1,14 @@
+       
+       public class VariableDeclaration {
+	    public static void main(String[] args) {
+	        // Declare and initialize variables of different data types
+	        int age = 25;
+	        double weight = 55.5;
+	        String name = "Abir Banerjee";
+
+	        // Print the values of the variables
+	        System.out.println("Name: " + name);
+	        System.out.println("Age: " + age);
+	        System.out.println("Weight: " + weight);
+	    }
+	}
